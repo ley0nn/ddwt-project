05b7e6729c2ff5c51d5066189d0af13dde2cca54
ley0nn <leongraumans@outlook.com>
2015-12-14 14:31:45 +0100

    Added gitignore + config file

---
+++ config.ini.php
@@ -0,0 +1,9 @@
+<?
+$con = mysql_connect("mysql01.service.rug.nl","s2732947","vishe4phoo");
+if (!$con) {
+die('Could not connect: ' . mysql_error());}
+ 
+mysql_select_db("s2732947", $con);
+
+
+?>
